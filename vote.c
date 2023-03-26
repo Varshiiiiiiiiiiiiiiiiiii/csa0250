30526d241a152f8fc7cc65896637c1d7faf0d36e
Varshiiiiiiiiiiiiiiiiiii <128604973+Varshiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2023-03-26 09:55:12 +0530

    Add files via upload

---
+++ vote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int age;
+	printf("Enter age :");
+	scanf("%d", &age);
+	
+	if (age >= 18)
+	    printf("You can vote!");
+	else
+	   printf("You cant vote!");
+	   
+	return 0;
+}
